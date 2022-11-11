5dbdd7a62b4309e4996d45ff535f79bdb6241e24
gguidoxx <gudombe@gmail.com>
2022-11-11 06:05:14 -0300

    cambios deploy

--- api/src/app.js
+++ api/src/app.js
@@ -27,7 +27,7 @@ server.use((req, res, next) => {
 });
 server.use(
   cors({
-    origin: LOCAL,
+    origin: process.env.LOCAL,
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     credentials: true,
   })
