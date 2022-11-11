13b74c70f2e14ebd6ac16f0d987a5017b016668b
gguidoxx <gudombe@gmail.com>
2022-11-10 21:40:55 -0300

    Cambios para deploy

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export function getNews() {
   return async function (dispatch) {
-    var json = await axios.get(`http://localhost:3001/`);
+    var json = await axios.get(process.env.REACT_APP_API_URL);
     return dispatch({
       type: "GET_NEWS",
       payload: json.data,
